8be2a563c5de1f1fb1463eb24866d660a44bb0b6
AurLemon <aurlemoncn@gmail.com>
2024-08-24 21:14:53 +0800

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  
+  configureWebpack: {
+    optimization: {
+      splitChunks: false, // 禁用代码拆分
+    },
+    output: {
+      filename: 'bundle.js', // 生成一个 JS 文件
+    }
+  },
+  
+  css: {
+    extract: {
+      filename: 'styles.css', // 将 CSS 提取到一个文件中
+    },
+  },
+});
